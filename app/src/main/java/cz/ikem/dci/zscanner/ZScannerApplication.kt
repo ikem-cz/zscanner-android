96da3de760ddd2f7f10661790ed5fa35bf22a947
Ales Teska <ales.teska@teskalabs.com>
2020-02-22 15:16:23 +0100

    Installation of the SeaCat client v3.

--- app/src/main/java/cz/ikem/dci/zscanner/ZScannerApplication.kt
+++ app/src/main/java/cz/ikem/dci/zscanner/ZScannerApplication.kt
@@ -4,20 +4,24 @@ import android.app.Application
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import java.util.concurrent.Executors
+import com.teskalabs.seacat.SeaCat
 
 class ZScannerApplication : Application() {
+
+    lateinit var seacat: SeaCat
+
     override fun onCreate() {
+        super.onCreate()
 
         val configuration = Configuration.Builder()
-                // Defines a thread pool with 10 threads.
-                // Ideally you would choose a number that is dynamic based on the number
-                // of cores on the device.
-                .setExecutor(Executors.newFixedThreadPool(2))
-                .build()
+            // Defines a thread pool with N threads.
+            // Ideally you would choose a number that is dynamic based on the number of cores on the device.
+            .setExecutor(Executors.newFixedThreadPool(2))
+            .build()
+
+        seacat = SeaCat(this, "http://10.0.2.2:8080/seacat/seacat")
 
         WorkManager.initialize(this, configuration)
-
-        super.onCreate()
     }
 
 
