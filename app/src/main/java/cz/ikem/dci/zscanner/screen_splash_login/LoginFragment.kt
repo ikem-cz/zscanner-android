304a808a05c3fe03065a6b6a6506d6ad8172cb97
Ola Wójcikowska <aleksandra.wojciowska@teskalabs.com>
2020-07-14 14:37:27 +0200

    Better handle nullability

--- app/src/main/java/cz/ikem/dci/zscanner/screen_splash_login/LoginFragment.kt
+++ app/src/main/java/cz/ikem/dci/zscanner/screen_splash_login/LoginFragment.kt
@@ -57,7 +57,9 @@ class LoginFragment : androidx.fragment.app.Fragment(), retrofit2.Callback<Respo
         fragmentView.login_progress_bar.visibility = INVISIBLE
         fragmentView.submit_button.visibility = VISIBLE
 
-        FailedLoginDialogFragment().show(fragmentManager!!, "failedLogin")
+        parentFragmentManager?.let{
+            FailedLoginDialogFragment().show(it, FailedLoginDialogFragment.TAG)
+        }
     }
 
     // Retrofit callback for a successful HTTP call
@@ -99,6 +101,8 @@ class LoginFragment : androidx.fragment.app.Fragment(), retrofit2.Callback<Respo
         fragmentView.login_progress_bar.visibility = INVISIBLE
         fragmentView.submit_button.visibility = VISIBLE
 
-        FailedLoginDialogFragment().show(fragmentManager!!, "failedLogin")
+        parentFragmentManager?.let{
+            FailedLoginDialogFragment().show(it, FailedLoginDialogFragment.TAG)
+        }
     }
 }
